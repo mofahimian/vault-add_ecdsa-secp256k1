96b6a575e8a4ebbc7f683435d55338405bd281da
mofahimian <36618771+mofahimian@users.noreply.github.com>
2022-04-20 14:33:01 +0430

    add hashes to ui

--- ui/app/helpers/sha2-digest-sizes.js
+++ ui/app/helpers/sha2-digest-sizes.js
@@ -1,6 +1,6 @@
 import { helper as buildHelper } from '@ember/component/helper';
 
-const SHA2_DIGEST_SIZES = ['sha2-224', 'sha2-256', 'sha2-384', 'sha2-512'];
+const SHA2_DIGEST_SIZES = ['sha2-224', 'sha2-256', 'sha2-384', 'sha2-512', 'keccak-256', 'double-sha2-256', 'none'];
 
 export function sha2DigestSizes() {
   return SHA2_DIGEST_SIZES;
